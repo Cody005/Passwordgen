b975778a07e5149c939ae27978ce59aab29e1361
Cody005 <72626458+Cody005@users.noreply.github.com>
2024-11-08 15:05:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+// Clear the concole on every refresh
 console.clear();
+// set the body to full height
+// document.body.style.height = `${innerHeight}px`
+
+// Range Slider Properties.
+// Fill : The trailing color that you see when you drag the slider.
+// background : Default Range Slider Background
 const sliderProps = {
 	fill: "#0B1EDF",
 	background: "rgba(255, 255, 255, 0.214)",
 };
 
+// Selecting the Range Slider container which will effect the LENGTH property of the password.
 const slider = document.querySelector(".range__slider");
 
+// Text which will show the value of the range slider.
 const sliderValue = document.querySelector(".length__title");
+
+// Using Event Listener to apply the fill and also change the value of the text.
 slider.querySelector("input").addEventListener("input", event => {
 	sliderValue.setAttribute("data-length", event.target.value);
 	applyFill(event.target);
 });
+// Selecting the range input and passing it in the applyFill func.
 applyFill(slider.querySelector("input"));
+// This function is responsible to create the trailing color and setting the fill.
 function applyFill(slider) {
 	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
 	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
@@ -153,9 +166,7 @@ function generatePassword(length, lower, upper, number, symbol) {
 			generatedPassword += randomFunc[funcName]();
 		});
 	}
-	return generatedPassword.slice(0, length)
-									.split('').sort(() => Math.random() - 0.5)
-									.join('');
+	return generatedPassword.slice(0, length);
 }
 
 // function that handles the checkboxes state, so at least one needs to be selected. The last checkbox will be disabled.
